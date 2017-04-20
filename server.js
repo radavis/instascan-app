45b3b5378443d574769ff475aec3ef15e68509b8
Richard Davis <rdavis.bacs@gmail.com>
2017-04-20 11:04:42 -0400

    log request ip

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ let server = express()
 let ipFilter = ((request, response, next) => {
   allowedIps = process.env.VALID_REQUEST_IPS.split(' ')
   requestIp = request.headers['x-forwarded-for'] || request.connection.remoteAddress
+  console.log(requestIp)
 
   if (!allowedIps.includes(requestIp)) {
     response.status(403).send('Forbidden')
