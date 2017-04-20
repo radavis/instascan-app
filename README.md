ea81fe322409035e34e3af97ac25edae65f74726
Richard Davis <rdavis.bacs@gmail.com>
2017-04-20 10:53:17 -0400

    prep for deployment

--- README.md
+++ README.md
@@ -8,7 +8,13 @@ Scan a QR code & post results to an API.
 $ git clone <this-repo>
 $ cd instascan-demo
 $ npm install
-$ npm start
+$ heroku local web  # loads .env for you
 ```
 
-Visit <http://localhost:3000/> and hold a QR code in front of the camera.
+Visit <http://localhost:5000/> and hold a QR code in front of the camera.
+
+## TODO
+
+* [x] Filter requests by IP.
+* [ ] Install instascan via NPM. Waiting on author to compile to ES5. [info](https://github.com/schmich/instascan/issues/42)
+* [ ] Integrate into LaunchPass application
--- server.js
+++ server.js
@@ -1,18 +1,36 @@
+let path = require('path')
 let express = require('express')
 let server = express()
-let port = 3000
 
-server.use(express.static('public'))
+let ipFilter = ((request, response, next) => {
+  allowedIps = process.env.VALID_REQUEST_IPS.split(' ')
+  requestIp = request.headers['x-forwarded-for'] || request.connection.remoteAddress
+
+  if (!allowedIps.includes(requestIp)) {
+    response.status(403).send('Forbidden')
+  } else {
+    next()
+  }
+})
+
+server.set('port', (process.env.PORT || 5000))
+server.use(ipFilter)
+server.use(express.static(path.join(__dirname, 'public')))
+
+// routes
+server.get('/', (request, response) => {
+  response.sendfile(path.join(__dirname, 'public', 'index.html' ))
+})
 
 server.post('/api/v1/attendance', (request, response) => {
   // always respond positively
   response.status(201).send('Created')
 })
 
-server.listen(port, '0.0.0.0', (error) => {
+// start
+server.listen(server.get('port'), (error) => {
   if (error) {
     console.log(error)
   }
-
-  console.info(`==> 🌎 Listening on port ${port}. Open http://0.0.0.0:${port}/ in your browser.`)
+  console.log(`==> 🌎  Listening on port ${server.get('port')}.`)
 })
